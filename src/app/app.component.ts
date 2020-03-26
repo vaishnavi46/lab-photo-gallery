e6c10cdfd67d5dee97e9113ea0e2dc22ffb9dc80
Teacher-Venkatesh <12venkat21@gmail.com>
2020-03-27 02:02:15 +0530

    lab-photo-gallery completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div class="container">
+  <div class="gallery">
+      <figure class="gallery__item gallery__item--1">
+          <img src="../assets/img/image-1.jpg" alt="Gallery image 1" class="gallery__img">
+      </figure>
+      <figure class="gallery__item gallery__item--2">
+          <img src="../assets/img/image-2.jpg" alt="Gallery image 2" class="gallery__img">
+      </figure>
+      <figure class="gallery__item gallery__item--3">
+          <img src="../assets/img/image-3.jpg" alt="Gallery image 3" class="gallery__img">
+      </figure>
+      <figure class="gallery__item gallery__item--4">
+          <img src="../assets/img/image-4.jpg" alt="Gallery image 4" class="gallery__img">
+      </figure>
+      <figure class="gallery__item gallery__item--5">
+          <img src="../assets/img/image-5.jpg" alt="Gallery image 5" class="gallery__img">
+      </figure>
+      <figure class="gallery__item gallery__item--6">
+          <img src="../assets/img/image-6.jpg" alt="Gallery image 6" class="gallery__img">
+      </figure>
+  </div>
+</div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lab-photo-gallery';
+}
